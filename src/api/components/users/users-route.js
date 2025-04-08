ea2aea86971cc299dbc5443865a9fa4a472882d5
WinnyJazz <jewinn2244@gmail.com>
2025-04-09 05:33:07 +0700

    Implement login endpoint for user authentication

--- src/api/components/users/users-route.js
+++ src/api/components/users/users-route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const usersController = require('./users-controller');
+const models = require("../../../models");
+const Users = models.Users;
 
 const route = express.Router();
 
@@ -12,6 +14,24 @@ module.exports = (app) => {
 
   // Create a new user
   route.post('/', usersController.createUser);
+
+  // log in endpoint
+  route.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await Users.findOne({ where: { email } }); // <-- ini yang benar!
+  
+      if (!user || user.password !== password) {
+        return res.status(403).json({ message: "INVALID_PASSWORD" });
+      }
+  
+      res.json({ message: "success" });
+    } catch (error) {
+      console.error(error); // ini bantu debugging juga
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
 
   // Get user detail
   route.get('/:id', usersController.getUser);
